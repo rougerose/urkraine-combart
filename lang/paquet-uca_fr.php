213b9477373e756a520a704afaa4a3bbb34e1da7
christophe le drean <christophe@rouge-rose.net>
2023-03-30 15:51:17 +0200

    Premier commit, avec un ensemble de base, non fonctionnel :
    - vitejs ne compile pas les fichiers scss correctement
    (résolution des url incorrectes avec @use).
    - Garder ces premiers développements pour mémoire, notamment l'ajout
    des fichiers du manifest via pipeline.

---
+++ lang/paquet-uca_fr.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+	'uca_description' => 'Squelettes et thème graphique du site Ukraine Combart',
+	'uca_nom' => 'Ukraine CombArt : squelettes et thème graphique',
+	'uca_slogan' => 'Squelettes et thème',
+	'uca_titre' => 'Ukraine CombArt',
+);
